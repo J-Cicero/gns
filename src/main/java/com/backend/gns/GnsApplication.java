ff2ba10cb71c8c3a16c5de4fa766aa4e8d28438b
J-Cicero <prudencioworou2006@gmail.com>
2026-04-07 10:11:48 +0000

    initial commit

---
+++ src/main/java/com/backend/gns/GnsApplication.java
@@ -0,0 +1,13 @@
+package com.backend.gns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/gns/GnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.gns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
