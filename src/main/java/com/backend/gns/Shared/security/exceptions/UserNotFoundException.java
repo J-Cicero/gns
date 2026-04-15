c33891fc754cb9a6a5e9388cbbc4ef9cb87f6446
J-Cicero <prudencioworou2006@gmail.com>
2026-04-15 18:05:46 +0000

    mise en place d'une branche exlusive a la logique metier entre les classes

--- src/main/java/com/backend/gns/Shared/security/exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.backend.gns.Shared.security.exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
