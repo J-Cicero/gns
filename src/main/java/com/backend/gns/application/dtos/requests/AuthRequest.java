d4d0a559cf099f4c442e68e4e4ad595a9b5832f1
J-Cicero <prudencioworou2006@gmail.com>
2026-05-10 14:14:30 +0000

    chore: suppression fichiers security - branche Jude propre

--- src/main/java/com/backend/gns/application/dtos/requests/AuthRequest.java
+++
@@ -1,9 +0,0 @@
-package com.backend.gns.application.dtos.requests;
-
-/**
- * DTO pour les requêtes d'authentification.
- */
-public record AuthRequest(
-        String email,
-        String password
-) {}
